e38e4319b7506322f942c84eaa01a9dbd7a9073d
Daisy Molving <daisymolving@gmail.com>
2015-08-21 17:44:49 +0100

    animated all buttons and made page jump to appropriate place on hover

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -16,11 +16,60 @@ function displayComments() {
 
 function showSupervisor() {
     $.when($(".invisible-button-supervisor").mouseover(function() {
-      $(".supervisor-bell-button").css("z-index", "10");
+      $(".supervisor-bell-button").css("z-index", "4");
       $(".supervisor-flag").css("z-index", "-1");
+      $("body").scrollTop(110);
     })).done($(".invisible-button-supervisor").mouseout(function(){
       $(".supervisor-bell-button").css("z-index", "-3");
       $(".supervisor-flag").css("z-index", "-4");
+  }));  
+}
+
+function showGallery() {
+  ($(".invisible-button-gallery").mouseover(function() {
+      $(".go-gallery").css("z-index", "-1");
+      $(".gallery-flag").css("z-index", "-1");
+      $("body").scrollTop(110);
+    }));
+  ($(".invisible-button-gallery").mouseout(function(){
+      $(".go-gallery").css("z-index", "-3");
+      $(".gallery-flag").css("z-index", "-4");
+  }));  
+}
+
+function showAbout() {
+  ($(".invisible-button-about").mouseover(function() {
+      $(".about_us_door_button").css("z-index", "1");
+      $(".about_us_flag").css("z-index", "-1");
+      $("body").scrollTop(110);
+    }));
+  ($(".invisible-button-about").mouseout(function(){
+      $(".about_us_door_button").css("z-index", "-3");
+      $(".about_us_flag").css("z-index", "-4");
+  }));  
+}
+
+function showNext() {
+  ($(".invisible-button-next").mouseover(function() {
+      $(".whats-next-button").css("z-index", "-1");
+      $(".whats-next-flag").css("z-index", "-1");
+      $("body").scrollTop(110);
+    }));
+  ($(".invisible-button-next").mouseout(function(){
+      $(".whats-next-button").css("z-index", "-3");
+      $(".whats-next-flag").css("z-index", "-4");
+  }));  
+}
+
+function showStuff() {
+  ($(".invisible-button-stuff").mouseover(function() {
+      $(".go-stuff-behind").css("z-index", "2");
+      $(".stuff-behind-flag").css("z-index", "-1");
+      $("body").scrollTop(110);
+    }));
+  ($(".invisible-button-stuff").mouseout(function(){
+      $(".go-stuff-behind").css("z-index", "-3");
+      $(".stuff-behind-flag").css("z-index", "-4");
   }));  
 }
 
@@ -28,9 +77,12 @@ function showSupervisor() {
 $(document).ready(function() {
 
   displayComments();
-  $(".supervisor-bell-button").css("z-index", "-3");
 
   showSupervisor();
+  showGallery();
+  showAbout();
+  showNext();
+  showStuff();
 
 
 
