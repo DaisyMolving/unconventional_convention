35d5b087b71762a5a0c76ccf19ed982d518b2d4c
Daisy Molving <daisymolving@gmail.com>
2015-08-21 19:51:41 +0100

    animated all sections that when relevant button at top of page is clicked the page scrolls down to the section

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -18,7 +18,7 @@ function showSupervisor() {
     $.when($(".invisible-button-supervisor").mouseover(function() {
       $(".supervisor-bell-button").css("z-index", "4");
       $(".supervisor-flag").css("z-index", "-1");
-      $("body").animate({ scrollTop: 110 }, "slow");
+      $("body").animate({ scrollTop: 130  }, "slow");
     })).done($(".invisible-button-supervisor").mouseout(function(){
       $(".supervisor-bell-button").css("z-index", "-3");
       $(".supervisor-flag").css("z-index", "-4");
@@ -29,7 +29,7 @@ function showGallery() {
   ($(".invisible-button-gallery").mouseover(function() {
       $(".go-gallery").css("z-index", "-1");
       $(".gallery-flag").css("z-index", "-1");
-      $("body").animate({ scrollTop: 110 }, "slow");
+      $("body").animate({ scrollTop: 130  }, "slow");
     }));
   ($(".invisible-button-gallery").mouseout(function(){
       $(".go-gallery").css("z-index", "-3");
@@ -41,7 +41,7 @@ function showAbout() {
   ($(".invisible-button-about").mouseover(function() {
       $(".about_us_door_button").css("z-index", "1");
       $(".about_us_flag").css("z-index", "-1");
-      $("body").animate({ scrollTop: 110 }, "slow");
+      $("body").animate({ scrollTop: 130  }, "slow");
     }));
   ($(".invisible-button-about").mouseout(function(){
       $(".about_us_door_button").css("z-index", "-3");
@@ -53,7 +53,7 @@ function showNext() {
   ($(".invisible-button-next").mouseover(function() {
       $(".whats-next-button").css("z-index", "-1");
       $(".whats-next-flag").css("z-index", "-1");
-      $("body").animate({ scrollTop: 110 }, "slow");
+      $("body").animate({ scrollTop: 130  }, "slow");
     }));
   ($(".invisible-button-next").mouseout(function(){
       $(".whats-next-button").css("z-index", "-3");
@@ -65,13 +65,44 @@ function showStuff() {
   ($(".invisible-button-stuff").mouseover(function() {
       $(".go-stuff-behind").css("z-index", "2");
       $(".stuff-behind-flag").css("z-index", "-1");
-      $("body").animate({ scrollTop: 110 }, "slow");
+      $("body").animate({ scrollTop: 130 }, "slow");
     }));
   ($(".invisible-button-stuff").mouseout(function(){
       $(".go-stuff-behind").css("z-index", "-3");
       $(".stuff-behind-flag").css("z-index", "-4");
   }));  
 }
+
+function scrollToAbout() {
+  ($(".invisible-button-about").click(function() {
+      $("body").animate({ scrollTop: 770 }, "slow");
+    }));
+
+}
+
+function scrollToSupervisor() {
+  ($(".invisible-button-supervisor").click(function() {
+      $("body").animate({ scrollTop: 4050 }, "slow");
+    }));
+} 
+
+function scrollToNext() {
+  ($(".invisible-button-next").click(function() {
+      $("body").animate({ scrollTop: 2570 }, "slow");
+    }));
+} 
+
+function scrollToStuff() {
+  ($(".invisible-button-stuff").click(function() {
+      $("body").animate({ scrollTop: 3270 }, "slow");
+    }));
+} 
+
+function scrollToGallery() {
+  ($(".invisible-button-gallery").click(function() {
+      $("body").animate({ scrollTop: 1550 }, "slow");
+    }));
+}  
 
 
 $(document).ready(function() {
@@ -84,6 +115,11 @@ $(document).ready(function() {
   showNext();
   showStuff();
 
+  scrollToGallery();
+  scrollToStuff();
+  scrollToNext();
+  scrollToSupervisor();
+  scrollToAbout();
 
 
 });
