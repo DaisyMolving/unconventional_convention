47b937270f0d476138376c65fc229d39d9ae9f95
Daisy Molving <daisymolving@gmail.com>
2015-08-21 16:38:38 +0100

    animated bell button that it glows on mouse-over and supervisor flag appears

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -14,11 +14,24 @@ function displayComments() {
   });
 }
 
+function showSupervisor() {
+    $.when($(".invisible-button-supervisor").mouseover(function() {
+      $(".supervisor-bell-button").css("z-index", "10");
+      $(".supervisor-flag").css("z-index", "-1");
+    })).done($(".invisible-button-supervisor").mouseout(function(){
+      $(".supervisor-bell-button").css("z-index", "-3");
+      $(".supervisor-flag").css("z-index", "-4");
+  }));  
+}
 
 
 $(document).ready(function() {
 
   displayComments();
+  $(".supervisor-bell-button").css("z-index", "-3");
+
+  showSupervisor();
+
 
 
 });
