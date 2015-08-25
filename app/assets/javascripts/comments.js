25792782c25c8cbec68bebf802b7a18e6079ab2d
Daisy Molving <daisymolving@gmail.com>
2015-08-25 17:04:49 +0100

    resized flags, first attempt at adding paralaxing unsuccessful, added skrollr rails to gemfile

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,4 +1,7 @@
 var publishedComments;
+var parallaxElements = [];
+var windowHeight = 0;
+
 
 function displayComments() {
   $.ajax({
@@ -104,8 +107,60 @@ function scrollToGallery() {
     }));
 }  
 
+function parallax(scrollTop) {
+
+  for (var id in parallaxElements) {
+
+        // distance of element from top of viewport
+    var viewportOffsetTop = parallaxElements[id].initialOffsetY - scrollTop;
+
+        // distance of element from bottom of viewport
+    var viewportOffsetBottom = windowHeight - viewportOffsetTop;
+
+    if ((viewportOffsetBottom >= parallaxElements[id].start) && (viewportOffsetBottom <= parallaxElements[id].stop)) {
+            // element is now active, fix the position so when we scroll it stays fixed
+
+      var speedMultiplier = parallaxElements[id].speed || 1;
+      var pos = (windowHeight - parallaxElements[id].start);
+
+      $(parallaxElements[id].elm)
+        .css({
+          position: 'fixed',
+          top: pos+'px',
+          left: '50%',
+          marginLeft: -(parallaxElements[id].width/2) +'px'
+        });
+
+    } else if (viewportOffsetBottom > parallaxElements[id].stop) {
+            // scrolled past the stop value, make position relative again
+
+      $(parallaxElements[id].elm)
+        .css({
+          position: 'relative',
+          top: (parallaxElements[id].stop-parallaxElements[id].start)+'px',
+          left: 'auto',
+          marginLeft: 'auto'
+        });
+
+    } else if (viewportOffsetBottom < parallaxElements[id].start) {
+            // scrolled up back past the start value, reset position
+
+      $(parallaxElements[id].elm)
+        .css({
+          position: 'relative',
+          top: 0,
+          left: 'auto',
+          marginLeft: 'auto'
+        });
+
+    }
+  }
+}
 
 $(document).ready(function() {
+
+  windowHeight = $(window).height();
+  $("body").animate({ scrollTop: 0  }, "slow");
 
   displayComments();
 
@@ -121,5 +176,26 @@ $(document).ready(function() {
   scrollToSupervisor();
   scrollToAbout();
 
+  var touchSupported = (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
 
+  if (touchSupported) {
+    $(window).bind('touchmove', function(e) {
+      var val = e.currentTarget.scrollY;
+        parallax(val);
+    });
+  }
+
+  $(window).bind('scroll', function(e) {
+    var val = $(this).scrollTop();
+      parallax(val);
+  });
+
+  $(window).resize(function() {
+    windowHeight = $(this).height();
+    for (var id in parallaxElements) {
+      parallaxElements[id].initialOffsetY = $(parallaxElements[id].elm).offset().top;
+      parallaxElements[id].height = $(parallaxElements[id].elm).height();
+    }
+  });
+   
 });
